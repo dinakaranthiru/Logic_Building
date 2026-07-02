4354bcae57f78650e2a0e32bdfd19677a8362631
Dina <dinakarant09@gmail.com>
2026-07-02 12:12:17 +0530

    Print 1 to 10 using while loop

---
+++ Logic_Building/While_Loop/Print1_to_10.py
@@ -0,0 +1,6 @@
+#print 1 to 10 using while loop
+i =1  # intialize a variable with value 1
+while i <11:  #if i is < 11 means go inside the  loop
+    print(i)  # then print the i
+    i = i+1   # after printing increase the value of i
+    
