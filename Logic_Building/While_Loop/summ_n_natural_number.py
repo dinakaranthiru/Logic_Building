1052af66bb3c36a45ba0acc6f57669603100d2ac
Dina <dinakarant09@gmail.com>
2026-07-02 15:15:24 +0530

    Print The sum of first n natural number

---
+++ Logic_Building/While_Loop/summ_n_natural_number.py
@@ -0,0 +1,8 @@
+# Calculate and print the sum of the first n natural numbers.
+n=70
+total_sum =0
+counter=1
+while counter <=n:
+    total_sum +=counter
+    counter+=1
+print('The sum is:',total_sum)    
