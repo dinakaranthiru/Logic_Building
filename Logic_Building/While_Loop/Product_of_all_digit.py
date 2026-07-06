279a9f9155268a0cca6d8b9183018cbed26c52bd
Dina <dinakarant09@gmail.com>
2026-07-06 15:51:34 +0530

    Product_of_all_digits

--- Logic_Building/While_Loop/Product_of_all_digit.py
+++ Logic_Building/While_Loop/Product_of_all_digit.py
@@ -4,7 +4,7 @@ num_str = abs(num)
 prod = 1 
 
 while num_str > 0:
-    dig = num_str % 10
-    prod = prod * dig
-    num_str = num_str //10
+    dig = num_str % 10  #give last digit (eg:4)
+    prod = prod * dig   #1*4
+    num_str = num_str //10 #remove the last digit
 print(prod)  
